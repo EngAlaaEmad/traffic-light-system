97d275030939235850202bc88c3b90f38cf1dca8
Alaa Emad <alaaemadhossney.ae@gmail.com>
2022-12-08 20:19:35 +0200

    Add files via upload

---
+++ On-demand Traffic light control/On-demand Traffic light control/main.c
@@ -0,0 +1,16 @@
+/*
+ * On-demand Traffic light control.c
+ *
+ * Created: 8/23/2022 11:23:13 PM
+ * Author : john
+ */ 
+
+#include "Application/Application.h"
+int main(void)
+{
+	APP_init();
+    while (1) 
+    {
+		APP_start();
+    }
+}
